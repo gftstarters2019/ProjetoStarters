e1100c87dd5145abc106a598510c9fa53b69a3cb
Vinicius Pierina <Vinicius.Pierina@gft.com>
2019-05-22 11:16:11 -0300

    Criando Interfaces de repositorios e criando o repositorio de contractholder

---
+++ Backend.Infrastructure/Repositories/ContractHolderRepository.cs
@@ -0,0 +1,46 @@
+﻿using Backend.Infrastructure.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Infrastructure.Repositories
+{
+    public class ContractHolderRepository// : IReadOnlyRepository<Individual>, IWriteRepository<Individual>
+    {
+        //private readonly ProjetoStartersContext _db;
+
+        //public ContractHolderRepository(ProjetoStartersContext db)
+        //{
+        //    _db = db;
+        //}
+
+        //public Individual Find(Guid id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public IEnumerable<Individual> Get()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Add(Individual individual)
+        //{
+        //    if (individual != null)
+        //    {
+        //        _db.Add(individual);
+        //        _db.SaveChanges();
+        //    }
+        //}
+
+        //public Individual Remove(Individual t)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Individual Update(Individual t)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
