7fe42a0470f54791c8290b2a92bff0c4c04c2bda
pauloleme <paulolemejr1@gmail.com>
2019-05-23 11:18:40 -0300

    front

---
+++ FrontEnd/ProjetoStarterFrontEnd/src/app/contract-holder/contract-holder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-holder',
+  templateUrl: './contract-holder.component.html',
+  styleUrls: ['./contract-holder.component.scss']
+})
+export class ContractHolderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
