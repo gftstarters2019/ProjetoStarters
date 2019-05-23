627b48d3b23ccfcd6fabf677696295af3f20820d
Vinicius Pierina <Vinicius.Pierina@gft.com>
2019-05-23 10:53:02 -0300

    Criando a a classe Function no Entry Point

---
+++ Function/IoC/InjectBindingProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function.IoC
+{
+    public class InjectBindingProvider : IBindingProvider
+    {
+        public readonly IServiceCollection ServiceCollection;
+
+        public InjectBindingProvider(IServiceCollection serviceProvider)
+        {
+            ServiceCollection = serviceProvider;
+        }
+        public Task<IBinding> TryCreateAsync(BindingProviderContext context)
+        {
+            LoadConfiguredDependencies(context);
+
+            ServiceProvider serviceProvider = ServiceCollection.BuildServiceProvider(true);
+
+            IBinding binding = new InjectBinding(serviceProvider, context.Parameter.ParameterType);
+            return Task.FromResult(binding);
+        }
+
+        private void LoadConfiguredDependencies(BindingProviderContext context)
+        {
+            var implementationType = context.Parameter.Member.DeclaringType.Assembly.GetExportedTypes()
+                                            .SingleOrDefault(t => typeof(IDependencyConfiguration).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+            if (implementationType == null)
+                return;
+
+            var dependencyConfig = (IDependencyConfiguration)Activator.CreateInstance(implementationType);
+            dependencyConfig.RegisterServices(ServiceCollection);
+        }
+    }
+}
---
+++ Function/IoC/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Function.IoC;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: WebJobsStartup(typeof(Startup), "A Web Jobs Startup Extension")]
+
+namespace Function.IoC
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var serviceCollection = builder.Services;
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            //Registering a filter
+            serviceCollection.AddSingleton<IFunctionFilter, ScopeCleanupFilter>();
+
+            serviceCollection.AddSingleton(new InjectBindingProvider(serviceCollection));
+
+            builder.AddExtension<InjectConfiguration>();
+
+            ConfigureServices(builder.Services, configuration);
+        }
+
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            //services.ConfigureServiceBus(new ServiceBusSettings(
+            //    configuration["ServiceBusConnectionString"], configuration["ServiceBusQueueName"],
+            //    configuration["ServiceBusTopicName"], configuration["ServiceBusSubscriptionName"]));
+        }
+    }
+}
