2f46f6e6987296221baec35402ab0ccbd43a65ee
ldbs <ldbs@gft.com>
2019-05-24 11:17:15 -0300

    Creating Simple Form

--- FrontEnd/ProjetoStarterFrontEnd/src/app/app.module.ts
+++ FrontEnd/ProjetoStarterFrontEnd/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContractHolderComponent } from './contract-holder/contract-holder.component';
 import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContractHolderComponent
+    ContractHolderComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [
     MaterialModule
