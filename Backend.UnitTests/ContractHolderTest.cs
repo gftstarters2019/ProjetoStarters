97ca1c9c1ae005dafcccc4f831c0003b9948c79b
ldbs <ldbs@gft.com>
2019-05-28 11:58:29 -0300

    Creating more Tests

--- Backend.UnitTests/ContractHolderTest.cs
+++ Backend.UnitTests/ContractHolderTest.cs
@@ -1,6 +1,5 @@
 using Backend.Core.Enums;
 using Backend.Core.Models;
-using Backend.UnitTests;
 using NUnit.Framework;
 using System;
 
@@ -8,25 +7,41 @@ namespace UnitTests
 {
     public class Tests
     {
-        public ObjectGenerationHelper ObjectGenerator { get; set; }
         [SetUp]
         public void Setup()
         {
-            ObjectGenerator = new ObjectGenerationHelper();
         }
 
         [Test]
         public void WhenCreateAnIndividual_ThenVerifyIfHeIsAContractHolder()
         {
             //arrange
-            var individual = ObjectGenerator.GenerateIndividual();
+            var individual = new Individual();
+            individual.BeneficiaryId = Guid.NewGuid();
+            individual.IndividualId = Guid.NewGuid();
+            individual.IndividualBirthdate = new DateTime(2019, 05, 28, 7, 0, 0);
+            individual.IndividualCPF = "45301152897";
+            individual.IndividualRG = "458559462";
+            individual.IndividualEmail = "gftstarters2019@outlook.com";
 
             //act
-            var contract = ObjectGenerator.GenerateContract(ContractType.LifeInsurance, ContractCategory.Gold);
-            var signedContract = ObjectGenerator.GenerateSignedContract(individual, contract);
+            var contract = new Contract();
+            contract.ContractId = Guid.NewGuid();
+            contract.ContractType = ContractType.HealthPlan;
+            contract.ContractCategory = ContractCategory.Diamond;
+            contract.ContractExpiryDate = new DateTime(2019, 05, 28, 7, 0, 0);
+            contract.ContractDeleted = false;
+
+            var signedContract = new SignedContract();
+            signedContract.ContractSignedId = Guid.NewGuid();
+            signedContract.IndividualId = individual.IndividualId;
+            signedContract.ContractId = contract.ContractId;
+            signedContract.ContractSignedIndividual = individual;
+            signedContract.ContractSignedContract = contract;
 
             //assert
             Assert.AreEqual(individual.IndividualId, signedContract.IndividualId);
+            Assert.AreEqual(individual, signedContract.ContractSignedIndividual);
 
         }
 
@@ -34,18 +49,40 @@ namespace UnitTests
         public void WhenCreateAContractHolder_ThenVerifyIfICanReadHisProperties()
         {
             //arrange
-            var beneficiaryId = new Guid("b83b58f8-d488-4181-86ab-1c065ef107ef");
-            var individualId = new Guid("3fa31ff9-a641-4831-9625-839009d558b4");
+            var beneficiaryId = new Guid("103660e3-5fd7-4606-bb1d-9d0f52e9c17a");
+            var individualId = new Guid("184ac189-467f-4e3b-badc-b6c299a25bc0");
             var individualBirthdate = new DateTime(2019, 05, 28, 7, 0, 0);
             var individualCPF = "45301152897";
             var individualRG = "458559462";
             var individualEmail = "gftstarters2019@outlook.com";
+
             //act
-            var individual = ObjectGenerator.GenerateIndividual();
-            var contract = ObjectGenerator.GenerateContract(ContractType.LifeInsurance, ContractCategory.Gold);
-            var signedContract = ObjectGenerator.GenerateSignedContract(individual, contract);
+            var individual = new Individual();
+            individual.BeneficiaryId = new Guid("103660e3-5fd7-4606-bb1d-9d0f52e9c17a");
+            individual.IndividualId = new Guid("184ac189-467f-4e3b-badc-b6c299a25bc0");
+            individual.IndividualBirthdate = new DateTime(2019, 05, 28, 7, 0, 0);
+            individual.IndividualCPF = "45301152897";
+            individual.IndividualRG = "458559462";
+            individual.IndividualEmail = "gftstarters2019@outlook.com";
+
+            var contract = new Contract();
+            contract.ContractId = Guid.NewGuid();
+            contract.ContractType = ContractType.HealthPlan;
+            contract.ContractCategory = ContractCategory.Diamond;
+            contract.ContractExpiryDate = new DateTime(2019, 05, 28, 7, 0, 0);
+            contract.ContractDeleted = false;
+
+            var signedContract = new SignedContract();
+            signedContract.ContractSignedId = Guid.NewGuid();
+            signedContract.IndividualId = individual.IndividualId;
+            signedContract.ContractId = contract.ContractId;
+            signedContract.ContractSignedIndividual = individual;
+            signedContract.ContractSignedContract = contract;
 
             //assert
+            Assert.AreEqual(individual.IndividualId, signedContract.IndividualId);
+            Assert.AreEqual(individual, signedContract.ContractSignedIndividual);
+
             Assert.AreEqual(individual.IndividualId, individualId);
             Assert.AreEqual(individual.BeneficiaryId, beneficiaryId);
             Assert.AreEqual(individual.IndividualBirthdate, individualBirthdate);
@@ -54,6 +91,133 @@ namespace UnitTests
             Assert.AreEqual(individual.IndividualEmail, individualEmail);
         }
 
-       
+        [Test]
+        public void WhenCreateAContractHolder_ThenVerifyIfICanFindById()
+        {
+            //arrange
+            var individual = new Individual();
+            individual.BeneficiaryId = Guid.NewGuid();
+            individual.IndividualId = Guid.NewGuid();
+            individual.IndividualBirthdate = new DateTime(2019, 05, 28, 7, 0, 0);
+            individual.IndividualCPF = "45301152897";
+            individual.IndividualRG = "458559462";
+            individual.IndividualEmail = "gftstarters2019@outlook.com";
+
+            var contract = new Contract();
+            contract.ContractId = Guid.NewGuid();
+            contract.ContractType = ContractType.HealthPlan;
+            contract.ContractCategory = ContractCategory.Diamond;
+            contract.ContractExpiryDate = new DateTime(2019, 05, 28, 7, 0, 0);
+            contract.ContractDeleted = false;
+
+            var signedContract = new SignedContract();
+            signedContract.ContractSignedId = Guid.NewGuid();
+            signedContract.IndividualId = individual.IndividualId;
+            signedContract.ContractId = contract.ContractId;
+            signedContract.ContractSignedIndividual = individual;
+            signedContract.ContractSignedContract = contract;
+
+            //act
+            var individualId = individual.IndividualId;
+
+            //assert
+            Assert.AreEqual(individual.IndividualId, signedContract.IndividualId);
+            Assert.AreEqual(individual, signedContract.ContractSignedIndividual);
+
+            Assert.AreEqual(individual.IndividualId, individualId);
+        }
+
+        [Test]
+        public void WhenCreateContractHolder_AndUpdateHim_ThenVerifyIfHeWasUpdated()
+        {
+            //arrange
+            var individual = new Individual();
+            individual.BeneficiaryId = Guid.NewGuid();
+            individual.IndividualId = Guid.NewGuid();
+            individual.IndividualBirthdate = new DateTime(2019, 05, 28, 7, 0, 0);
+            individual.IndividualCPF = "45301152897";
+            individual.IndividualRG = "458559462";
+            individual.IndividualEmail = "gftstarters2019@outlook.com";
+
+            var contract = new Contract();
+            contract.ContractId = Guid.NewGuid();
+            contract.ContractType = ContractType.HealthPlan;
+            contract.ContractCategory = ContractCategory.Diamond;
+            contract.ContractExpiryDate = new DateTime(2019, 05, 28, 7, 0, 0);
+            contract.ContractDeleted = false;
+
+            var signedContract = new SignedContract();
+            signedContract.ContractSignedId = Guid.NewGuid();
+            signedContract.IndividualId = individual.IndividualId;
+            signedContract.ContractId = contract.ContractId;
+            signedContract.ContractSignedIndividual = individual;
+            signedContract.ContractSignedContract = contract;
+
+            var updatedIndividual = new Individual();
+
+            var beneficiaryId = Guid.NewGuid();
+            var individualId = Guid.NewGuid();
+            var individualBirthdate = new DateTime();
+            var individualCPF = "45301152898";
+            var individualRG = "458559466";
+            var individualEmail = "gftstarters@outlook.com";
+
+            //act
+            updatedIndividual.BeneficiaryId = beneficiaryId;
+            updatedIndividual.IndividualId = individualId;
+            updatedIndividual.IndividualBirthdate = individualBirthdate;
+            updatedIndividual.IndividualCPF = individualCPF;
+            updatedIndividual.IndividualRG = individualRG;
+            updatedIndividual.IndividualEmail = individualEmail;
+
+            //assert
+            Assert.AreEqual(individual.IndividualId, signedContract.IndividualId);
+            Assert.AreEqual(individual, signedContract.ContractSignedIndividual);
+
+            Assert.AreNotEqual(individual.IndividualId, updatedIndividual.IndividualId);
+            Assert.AreNotEqual(individual.BeneficiaryId, updatedIndividual.BeneficiaryId);
+            Assert.AreNotEqual(individual.IndividualBirthdate, updatedIndividual.IndividualBirthdate);
+            Assert.AreNotEqual(individual.IndividualCPF, updatedIndividual.IndividualCPF);
+            Assert.AreNotEqual(individual.IndividualRG, updatedIndividual.IndividualRG);
+            Assert.AreNotEqual(individual.IndividualEmail, updatedIndividual.IndividualEmail);
+        }
+
+
+        [Test]
+        public void WhenCreateAContractHolder_AndDeleteHim_ThenVerifyIfHeWasDeleted()
+        {
+            //arrange
+            var individual = new Individual();
+            individual.BeneficiaryId = Guid.NewGuid();
+            individual.IndividualId = Guid.NewGuid();
+            individual.IndividualBirthdate = new DateTime(2019, 05, 28, 7, 0, 0);
+            individual.IndividualCPF = "45301152897";
+            individual.IndividualRG = "458559462";
+            individual.IndividualEmail = "gftstarters2019@outlook.com";
+
+            var contract = new Contract();
+            contract.ContractId = Guid.NewGuid();
+            contract.ContractType = ContractType.HealthPlan;
+            contract.ContractCategory = ContractCategory.Diamond;
+            contract.ContractExpiryDate = new DateTime(2019, 05, 28, 7, 0, 0);
+            contract.ContractDeleted = false;
+
+            var signedContract = new SignedContract();
+            signedContract.ContractSignedId = Guid.NewGuid();
+            signedContract.IndividualId = individual.IndividualId;
+            signedContract.ContractId = contract.ContractId;
+            signedContract.ContractSignedIndividual = individual;
+            signedContract.ContractSignedContract = contract;
+
+            //act
+            signedContract = null;
+            contract = null;
+            individual = null;
+
+            //assert
+            Assert.IsNull(signedContract);
+            Assert.IsNull(contract);
+            Assert.IsNull(individual);
+        }
     }
 }
