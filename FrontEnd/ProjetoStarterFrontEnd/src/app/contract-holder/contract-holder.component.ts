afb126d66da450879a64e64d30506009f7f3aeb2
pauloleme <paulolemejr1@gmail.com>
2019-05-31 13:21:59 -0300

    front

--- FrontEnd/ProjetoStarterFrontEnd/src/app/contract-holder/contract-holder.component.ts
+++ FrontEnd/ProjetoStarterFrontEnd/src/app/contract-holder/contract-holder.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit, ViewChild, AfterViewInit, Output, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Validators, FormBuilder, FormArray, FormGroup } from '@angular/forms';
-import { GridOptions, ColDef, SelectionChangedEvent } from 'ag-grid-community';
-
-import { CellDatepickerComponent } from '../cell-datepicker/cell-datepicker.component';
-import * as moment from 'moment';
-
+import { GridOptions, ColDef, RowSelectedEvent } from 'ag-grid-community';
+import "ag-grid-enterprise";
 
 @Component({
   selector: 'app-contract-holder',
@@ -16,14 +13,16 @@ export class ContractHolderComponent implements OnInit, AfterViewInit {
 
   private columnDefs: Array<ColDef>;
   private rowData;
-
+  private paginationPageSize;
+  detailCellRendererParams;
+  gridApi;
+  gridColumApi;
 
 
   gridOptions: GridOptions;
   load_failure: boolean;
+  contractHolder: FormGroup;
 
-  Gender_: string;
-  genders: string[] = ['Male   ', 'Female'];
   showList: boolean = true;
   showAddresslist: boolean = false;
 
@@ -32,8 +31,11 @@ export class ContractHolderComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.setup_gridOptions();
     this.setup_gridData();
+    this.setup_gridOptions();
+    this.setup_form();
+
+
   }
 
   ngAfterViewInit() {
@@ -41,41 +43,48 @@ export class ContractHolderComponent implements OnInit, AfterViewInit {
   }
 
 
+  private setup_form() {
+    this.contractHolder = this.chfb.group({
+      name: ['', Validators.required],
+      rg: ['', Validators.required],
+      cpf: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      email: ['', Validators.required],
+  
+      idAddress: this.chfb.array([
+        this.chfb.group({
+          street: ['', Validators.required],
+          type: ['', Validators.required],
+          number: ['', Validators.required],
+          state: ['', Validators.required],
+          neighborhood: ['', Validators.required],
+          country: ['', Validators.required],
+          zipCode: ['', Validators.required],
+        })
+      ]),
+  
+    });
+  }
 
-  contractHolder = this.chfb.group({
-    name: ['', Validators.required],
-    genders: ['', Validators.required],
-    rg: ['', Validators.required],
-    cpf: ['', Validators.required],
-    birthdate: ['', Validators.required],
-    email: ['', Validators.required],
-    // idAddress: ['', Validators.required]
-
-    idAddress: this.chfb.array([
-      this.chfb.group({
-        street: ['', Validators.required],
-        type: ['', Validators.required],
-        number: ['', Validators.required],
-        state: ['', Validators.required],
-        neighborhood: ['', Validators.required],
-        country: ['', Validators.required],
-        zipCode: ['', Validators.required],
-      })
-    ]),
-
-  });
 
 
   onSubmit(): void {
     console.log(this.contractHolder.value);
+
+   let json = JSON.stringify(this.contractHolder.value);
+   let httpOptions = {headers: new HttpHeaders ({
+     'Content-Type': 'application/json'
+   })};
+   this.http.post('https://contractholderwebapi.azurewebsites.net/api/ContractHolder', json,httpOptions).subscribe(data => console.log(data));
+   
   }
+  
 
   showButton() {
     this.showList = !this.showList;
   }
 
   showAddress() {
-    debugger;
     const addressControl = this.contractHolder.controls.idAddress as FormArray;
     const hasMax = addressControl.length >= 5;
 
@@ -101,6 +110,8 @@ export class ContractHolderComponent implements OnInit, AfterViewInit {
     }))
   }
 
+
+
   handle_add($event: any) {
     debugger;
   }
@@ -110,54 +121,183 @@ export class ContractHolderComponent implements OnInit, AfterViewInit {
   private setup_gridOptions() {
     this.gridOptions = {
       rowSelection: 'single',
+
       onRowSelected: this.onRowSelected.bind(this),
+      masterDetail: true,
 
       columnDefs: [
         {
-          headerName: 'Name', field: 'name', lockPosition: true, sortable: true, filter: true,
-          editable: true, onCellValueChanged: this.onCellEdit.bind(this)
+          headerName: 'Name',
+          field: 'name',
+          lockPosition: true,
+          sortable: true,
+          filter: true,
+          onCellValueChanged:
+            this.onCellEdit.bind(this)
         },
+
         {
-          headerName: 'CPF', field: 'cpf', lockPosition: true, sortable: true, filter: true,
-          editable: true, onCellValueChanged: this.onCellEdit.bind(this),
+          headerName: 'CPF',
+          field: 'cpf',
+          lockPosition: true,
+          sortable: true,
+          filter: true,
+          editable: true,
+          onCellValueChanged: this.onCellEdit.bind(this),
         },
+
         {
-          headerName: 'RG', field: 'rg', lockPosition: true, editable: true,
+          headerName: 'RG',
+          field: 'rg',
+          lockPosition: true,
+          editable: true,
+          onCellValueChanged: this.onCellEdit.bind(this)
+        },
+
+        {
+          headerName: 'Birthdate',
+          field: 'birthdate',
+          lockPosition: true,
+          sortable: true,
+          editable: true,
+          onCellValueChanged: this.onCellEdit.bind(this),
+          // valueFormatter: (data) => data.value ? moment(data.value).format('L') : null,
+        },
+
+        {
+          headerName: 'Email',
+          field: 'email',
+          lockPosition: true,
+          sortable: true,
+          editable: true,
           onCellValueChanged: this.onCellEdit.bind(this)
         },
         {
-          headerName: 'Gender', field: 'gender', lockPosition: true, editable: true,
-          cellEditor: "agRichSelectCellEditor", cellEditorParams: {
-            cellHeight: 50,
-            values: ["Male", "Female"]
-          }
-        },
-        {
-          headerName: 'Birthdate', field: 'birthdate', lockPosition: true, sortable: true, editable: true,
-          cellRendererFramework: CellDatepickerComponent,
-          cellEditorFramework: CellDatepickerComponent,
-          valueFormatter: (data) => data.value ? moment(data.value).format('L') : null,
-        },
-        {
-          headerName: 'Email', field: 'email', lockPosition: true, sortable: true, editable: true,
-          onCellValueChanged: this.onCellEdit.bind(this)
-        },
+          headerName: 'Edit/Delete',
+          field: 'editDelete',
+          colId: "params",
+          width: 60,
+          lockPosition: true,
+        }
       ],
+
+      detailCellRendererParams: {
+        detailGridOptions: {
+          columnDefs: [
+            { field: "street" },
+            { field: "type" },
+            { field: "number" },
+            { field: "state" },
+            { field: "neighborhood" },
+            { field: "country" },
+            { field: "zipcode" }
+          ],
+
+          onFirstDataRendered(params) {
+            params.api.sizeColumnsToFit();
+          }
+          
+        },
+        
+        getDetailRowData: function (params) {
+          debugger;
+          params.successCallback(params.data.idAddress);
+        },
+
+        template: function(params) {
+        
+          var personName = params.data.name;
+          debugger;
+          return (
+            '<div style="height: 100%; background-color: #EDF6FF; padding: 20px; box-sizing: border-box;">' +
+            '  <div style="height: 10%;">Name: ' +
+            personName +
+            "</div>" +
+            '  <div ref="eDetailGrid" style="height: 90%;"></div>' +
+            "</div>"
+          );
+        }
+        
+
+
+
+      },
 
       rowData: [
-        { name: 'Paulo', cpf: 333333, rg: 444444, gender: 'Male', birthdate: '12/08/1995', email: 'paulo@gft.com' },
-        { name: 'Ariel', cpf: 111111, rg: 666666, gender: 'Male', birthdate: '12/08/1996', email: 'ariel@gft.com' },
-        { name: 'Andre', cpf: 666666, rg: 111111, gender: 'Male', birthdate: '12/08/1994', email: 'andre@gft.com' },
-        { name: 'Gilberto', cpf: 222222, rg: 555555, gender: 'Male', birthdate: '12/08/1991', email: 'gilberto@gft.com' },
-        { name: 'Vinicius', cpf: 444444, rg: 333333, gender: 'Male', birthdate: '12/08/1996', email: 'vinicius@gft.com' },
-        { name: 'Leonardo', cpf: 555555, rg: 222222, gender: 'Male', birthdate: '12/08/1996', email: 'leonardo@gft.com' }
+        {
+          name: 'Paulo',
+          cpf: 333333,
+          rg: 444444,
+          birthdate: '12/08/1995',
+          email: 'paulo@gft.com',
+          idAddress: [{
+            street: 'dont care',
+            type: 'really dont care',
+            number: 123,
+            state: 'still dont care',
+            neighborhood: '',
+            country: '',
+            zipCode: '',
+          }]
+        },
+
+        {
+          name: 'Ariel',
+          cpf: 111111,
+          rg: 666666,
+          birthdate: '12/08/1996',
+          email: 'ariel@gft.com'
+        },
+
+        {
+          name: 'Andre',
+          cpf: 666666,
+          rg: 111111,
+          birthdate: '12/08/1994',
+          email: 'andre@gft.com'
+        },
+
+        {
+          name: 'Gilberto',
+          cpf: 222222,
+          rg: 555555,
+          birthdate: '12/08/1991',
+          email: 'gilberto@gft.com'
+        },
+
+        {
+          name: 'Vinicius',
+          cpf: 444444,
+          rg: 333333,
+          birthdate: '12/08/1996',
+          email: 'vinicius@gft.com'
+        },
+
+        {
+          name: 'Leonardo',
+          cpf: 555555,
+          rg: 222222,
+          birthdate: '12/08/1996',
+          email: 'leonardo@gft.com'
+        }
       ],
 
+      onGridReady: this.onGridReady.bind(this)
     }
+
+
+
+
   }
 
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumApi = params.columnApi;
+  }
+
+
   private setup_gridData() {
-    this.rowData = this.http.get('https://contractholderwebapi.azurewebsites.net/').subscribe(
+    this.rowData = this.http.get('https://contractholderwebapi.azurewebsites.net/api/ContractHolder').subscribe(
       value => {
         this.rowData = value;
       },
@@ -172,17 +312,25 @@ export class ContractHolderComponent implements OnInit, AfterViewInit {
   private onCellEdit(params: any) {
     console.log(params.newValue);
     console.log(params.data);
+
   }
 
-  private onRowSelected(event: SelectionChangedEvent) {
-    const { } = event;
-    debugger;
+  private onRowSelected(event: RowSelectedEvent) {
+    const { data } = event;
+    this.contractHolder.getRawValue();
+    console.log(data);
+    //debugger;
+    this.contractHolder.patchValue(data);
+    
+    console.log(data);
+    //debugger;
   }
 
+}
   // private hideGridLoading() {
   //   this.gridOptions.api.setRowData([]);
   // }
-}
+
 
 
 
