5a92f3ed11c02bab42325885ec656992c9f3f245
Ariel Molina <ariel.molina@gft.com>
2019-06-06 14:43:39 -0300

    API returning address with Realty GET Request

---
+++ Backend.Application/ViewModels/RealtyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Application.ViewModels
+{
+    public class RealtyViewModel
+    {
+        public string RealtyMunicipalRegistration { get; set; }
+        public DateTime RealtyConstructionDate { get; set; }
+        public double RealtySaleValue { get; set; }
+        public double RealtyMarketValue { get; set; }
+        public Address RealtyAddress { get; set; }
+    }
+}
--- Backend.Infrastructure/Repositories/Beneficiaries/RealtyRepository.cs
+++ Backend.Infrastructure/Repositories/Beneficiaries/RealtyRepository.cs
@@ -1,4 +1,5 @@
-﻿using Backend.Core.Models;
+﻿using Backend.Application.ViewModels;
+using Backend.Core.Models;
 using Backend.Infrastructure.Configuration;
 using Backend.Infrastructure.Repositories.Contracts;
 using System;
@@ -8,7 +9,7 @@ using System.Text;
 
 namespace Backend.Infrastructure.Repositories
 {
-    public class RealtyRepository : IReadOnlyRepository<Realty>, IWriteRepository<Realty>
+    public class RealtyRepository : IReadOnlyRepository<RealtyViewModel>, IWriteRepository<Realty>
     {
         private readonly ConfigurationContext _db;
 
@@ -27,17 +28,7 @@ namespace Backend.Infrastructure.Repositories
             }
             return false;
         }
-
-        public Realty Find(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Realty> Get() => _db
-            .Realties
-            .Where(i => !i.IsDeleted)
-            .ToList();
-
+        
         public bool Remove(Guid id)
         {
             throw new NotImplementedException();
@@ -46,6 +37,35 @@ namespace Backend.Infrastructure.Repositories
         public Realty Update(Guid id, Realty t)
         {
             throw new NotImplementedException();
+        }
+
+        public RealtyViewModel Find(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RealtyViewModel> Get()
+        {
+            List<RealtyViewModel> realtiesToReturn = new List<RealtyViewModel>();
+            var realties = _db.Realties.ToList();
+            foreach(var realty in realties)
+            {
+                realtiesToReturn.Add(new RealtyViewModel()
+                {
+                    RealtyConstructionDate = realty.RealtyConstructionDate,
+                    RealtyMarketValue = realty.RealtyMarketValue,
+                    RealtyMunicipalRegistration = realty.RealtyMunicipalRegistration,
+                    RealtySaleValue = realty.RealtySaleValue,
+                    RealtyAddress = _db
+                                    .Addresses
+                                    .Where(a => a.AddressId == _db.Beneficiary_Address
+                                                                .Where(ba => ba.BeneficiaryId == realty.BeneficiaryId)
+                                                                .Select(ba => ba.AddressId)
+                                                                .FirstOrDefault())
+                                    .FirstOrDefault()
+                });
+            }
+            return realtiesToReturn;
         }
     }
 }
