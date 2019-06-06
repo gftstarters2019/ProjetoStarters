de85fc9201c66d9c7d6efee91db77f3f2853fd30
Ariel Molina <ariel.molina@gft.com>
2019-06-06 15:13:50 -0300

    Changed SignedContractID column name migration.

--- Backend.Infrastructure/Migrations/20190606181031_ChangeColumnName.Designer.cs
+++ Backend.Infrastructure/Migrations/20190606181031_ChangeColumnName.Designer.cs
@@ -4,14 +4,16 @@ using Backend.Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(ConfigurationContext))]
-    partial class ConfigurationContextModelSnapshot : ModelSnapshot
+    [Migration("20190606181031_ChangeColumnName")]
+    partial class ChangeColumnName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
