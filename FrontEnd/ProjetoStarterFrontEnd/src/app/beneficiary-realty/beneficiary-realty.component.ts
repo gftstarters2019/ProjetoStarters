45e8a654c52bbc6839ad5c7eb96e23af1ac17c7b
GilbertoYabiku <gilbertoyabikujun@gmail.com>
2019-06-11 15:27:49 -0300

    Fix Contract Post

--- FrontEnd/ProjetoStarterFrontEnd/src/app/beneficiary-realty/beneficiary-realty.component.ts
+++ FrontEnd/ProjetoStarterFrontEnd/src/app/beneficiary-realty/beneficiary-realty.component.ts
@@ -1,7 +1,5 @@
-import { AddressComponent } from './../address/address.component';
-import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
-import { FormControl, Validators, FormBuilder } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, Output, EventEmitter, Input, SimpleChanges } from '@angular/core';
+import { FormControl, Validators, FormBuilder, AbstractControl, FormGroup } from '@angular/forms';
 import { GenericValidator } from '../Validations/GenericValidator';
 
 @Component({
@@ -11,7 +9,9 @@ import { GenericValidator } from '../Validations/GenericValidator';
 })
 export class BeneficiaryRealtyComponent implements OnInit {
 
-  @ViewChild(AddressComponent) address;
+  @Input() realtyForm: FormGroup;
+
+  @Input() realtyPushPermission !: number;
 
   @Output() messageRealtyEvent = new EventEmitter<any>();
 
@@ -20,28 +20,44 @@ export class BeneficiaryRealtyComponent implements OnInit {
     realtyMunicipalRegistration: new FormControl('', Validators.pattern(GenericValidator.regexSimpleName)),
     realtyConstructionDate: new FormControl('', GenericValidator.dateValidation()),
     realtySaleValue: new FormControl('', GenericValidator.negativeValidation()),
-    realtyMarketValue: new FormControl('', GenericValidator.negativeValidation())
+    realtyMarketValue: new FormControl('', GenericValidator.negativeValidation()),
+    addressStreet: ['', Validators.pattern(GenericValidator.regexSimpleName)],
+    addressType: ['', Validators.required],
+    addressNumber: ['', [Validators.pattern(/^[0-9]+$/), Validators.maxLength(4)]],
+    addressState: ['', [Validators.pattern(/^[[A-Z]+$/), Validators.maxLength(2), Validators.minLength(2)]],
+    addressNeighborhood: [ '', Validators.pattern(GenericValidator.regexSimpleName)],
+    addressCountry: ['', Validators.pattern(GenericValidator.regexSimpleName)],
+    addressZipCode: ['', Validators.required],
+    addressCity: [''],
+    addressComplement: ['']
   });
 
-  constructor(private _httpClient: HttpClient, private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
-  response:any;
-  public realtyPost(): void{
-    let form = JSON.stringify(this.realtyCreateForm.value);
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json'
-      })
-    };
-    this._httpClient.post('https://beneficiarieswebapi.azurewebsites.net/api/Beneficiary/Realty', form, httpOptions)
-    .subscribe(data => { 
-      this.response = data;
-      if(this.response != null){
-        this.messageRealtyEvent.emit(this.response.beneficiaryId);
-      }
-    });
+  unMaskValues(): void {
+    let zipCode = this.realtyCreateForm.controls.addressZipCode.value;
+    zipCode = zipCode.replace(/\D+/g, '');
+    this.realtyCreateForm.controls.addressZipCode.setValue(zipCode);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.realtyPushPermission.currentValue != 0 && changes.realtyPushPermission.currentValue != changes.realtyPushPermission.previousValue) {
+      this.unMaskValues();
+      this.messageRealtyEvent.emit(this.realtyCreateForm);
+    }
+  }
+
+  zipCodeValidation(control: AbstractControl): {[key: string]: boolean} | null {
+    let zipCodeNumber = control.value;
+
+    zipCodeNumber = zipCodeNumber.replace(/\D+/g, '');
+
+    if(zipCodeNumber.length < 8)
+      return {"zipCodeIsTooShort": true};
+    
+    return null;
   }
 }
