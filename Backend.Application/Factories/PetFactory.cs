c5eec58ea39f7babcab3ae74b0464fbb5d019af1
Ariel Molina <ariel.molina@gft.com>
2019-06-13 12:30:17 -0300

    Created Validations classes and reworked ContractViewModelRepository to add Beneficiaries to DB from ContractViewModel.

--- Backend.Application/Factories/PetFactory.cs
+++ Backend.Application/Factories/PetFactory.cs
@@ -13,41 +13,12 @@ namespace Backend.Application.Factories
 
         public List<Pet> CreateList(List<Pet> pets)
         {
-            foreach (var pet in pets)
-            {
-                if (PetIsValid(pet))
-                {
-
-                }
-            }
-                throw new NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public Pet Create(ContractViewModel vm)
         {
-            //if(PetIsValid())
             throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Verifies if Pet is valid
-        /// </summary>
-        /// <param name="pet">Pet to be verified</param>
-        /// <returns>If Pet is valid</returns>
-        public static bool PetIsValid(Pet pet)
-        {
-            if (!DateIsValid(pet.PetBirthdate))
-                return false;
-            return true;
-        }
-
-        /// <summary>
-        /// Verifies if date is not future date
-        /// </summary>
-        /// <returns>If date is valid</returns>
-        public static bool DateIsValid(DateTime date)
-        {
-            return date != null ? date > DateTime.Today : false;
         }
     }
 }
