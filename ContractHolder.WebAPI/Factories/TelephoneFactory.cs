af8029701b147292ad176f3fe110c3ca53fe4c29
Vinicius Pierina <Vinicius.Pierina@gft.com>
2019-06-19 13:35:26 -0300

    Fazendo as factories de Contract Holder

---
+++ ContractHolder.WebAPI/Factories/TelephoneFactory.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Core.Models;
+using ContractHolder.WebAPI.Factories.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ContractHolder.WebAPI.Factories
+{
+    public class TelephoneFactory : IFactoryList<Telephone>
+    {
+        private List<Telephone> telephones = null;
+        private Telephone telephone = null;
+
+        public TelephoneFactory()
+        {
+            telephones = new List<Telephone>();
+        }
+
+        public List<Telephone> CreateList(List<Telephone> vm_telephones)
+        {
+            telephones = new List<Telephone>();
+            foreach (var tel in vm_telephones)
+            {
+                telephone = new Telephone();
+
+                telephone.TelephoneId = Guid.NewGuid();
+                telephone.TelephoneNumber = tel.TelephoneNumber;
+                telephone.TelephoneType = tel.TelephoneType;
+
+                telephones.Add(telephone);
+            }
+
+            if (telephones.Count != vm_telephones.Count || telephones.Count > 5)
+            {
+                return null;
+            }
+
+            return telephones;
+        }
+    }
+}
