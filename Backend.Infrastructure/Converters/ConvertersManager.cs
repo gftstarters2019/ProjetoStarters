28a650cd776a2add953f6d71a73df0c17b5a65ed
Ariel Molina <ariel.molina@gft.com>
2019-06-20 11:18:54 -0300

    Converters creation.

---
+++ Backend.Infrastructure/Converters/ConvertersManager.cs
@@ -0,0 +1,163 @@
+﻿namespace Backend.Infrastructure.Converters
+{
+    public sealed class ConvertersManager
+    {
+        private static ContractConverter contractConverter = null;
+        private static SignedContractConverter signedContractConverter = null;
+        private static IndividualConverter individualConverter = null;
+        private static PetConverter petConverter = null;
+        private static MobileDeviceConverter mobileDeviceConverter = null;
+        private static RealtyConverter realtyConverter = null;
+        private static VehicleConverter vehicleConverter = null;
+        private static AddressConverter addressConverter = null;
+        private static readonly object padlock = new object();
+
+        public ConvertersManager()
+        {
+        }
+
+        public static ContractConverter ContractConverter
+        {
+            get
+            {
+                if(contractConverter == null)
+                {
+                    lock(padlock)
+                    {
+                        if(contractConverter == null)
+                        {
+                            contractConverter = new ContractConverter();
+                        }
+                    }
+                }
+                return contractConverter;
+            }
+        }
+
+        public static SignedContractConverter SignedContractConverter
+        {
+            get
+            {
+                if (signedContractConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (signedContractConverter == null)
+                        {
+                            signedContractConverter = new SignedContractConverter();
+                        }
+                    }
+                }
+                return signedContractConverter;
+            }
+        }
+
+        public static IndividualConverter IndividualConverter
+        {
+            get
+            {
+                if (individualConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (individualConverter == null)
+                        {
+                            individualConverter = new IndividualConverter();
+                        }
+                    }
+                }
+                return individualConverter;
+            }
+        }
+
+        public static PetConverter PetConverter
+        {
+            get
+            {
+                if (petConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (petConverter == null)
+                        {
+                            petConverter = new PetConverter();
+                        }
+                    }
+                }
+                return petConverter;
+            }
+        }
+
+        public static MobileDeviceConverter MobileDeviceConverter
+        {
+            get
+            {
+                if (mobileDeviceConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (mobileDeviceConverter == null)
+                        {
+                            mobileDeviceConverter = new MobileDeviceConverter();
+                        }
+                    }
+                }
+                return mobileDeviceConverter;
+            }
+        }
+
+        public static RealtyConverter RealtyConverter
+        {
+            get
+            {
+                if (realtyConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (realtyConverter == null)
+                        {
+                            realtyConverter = new RealtyConverter();
+                        }
+                    }
+                }
+                return realtyConverter;
+            }
+        }
+
+        public static VehicleConverter VehicleConverter
+        {
+            get
+            {
+                if (vehicleConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (vehicleConverter == null)
+                        {
+                            vehicleConverter = new VehicleConverter();
+                        }
+                    }
+                }
+                return vehicleConverter;
+            }
+        }
+
+        public static AddressConverter AddressConverter
+        {
+            get
+            {
+                if (addressConverter == null)
+                {
+                    lock (padlock)
+                    {
+                        if (addressConverter == null)
+                        {
+                            addressConverter = new AddressConverter();
+                        }
+                    }
+                }
+                return addressConverter;
+            }
+        }
+    }
+}
---
+++ Backend.Infrastructure/Converters/RealtyConverter.cs
@@ -0,0 +1,47 @@
+﻿using Backend.Core.Domains;
+using Backend.Core.Models;
+using Backend.Infrastructure.Converters.Interfaces;
+
+namespace Backend.Infrastructure.Converters
+{
+    public class RealtyConverter : IConverter<RealtyDomain, RealtyEntity>
+    {
+        public RealtyDomain Convert(RealtyEntity realtyEntity)
+        {
+            if (realtyEntity == null)
+                return null;
+
+            var realtyDomain = new RealtyDomain()
+            {
+                BeneficiaryId = realtyEntity.BeneficiaryId,
+                IsDeleted = realtyEntity.IsDeleted,
+                RealtyConstructionDate = realtyEntity.RealtyConstructionDate,
+                RealtyMarketValue = realtyEntity.RealtyMarketValue,
+                RealtyMunicipalRegistration = realtyEntity.RealtyMunicipalRegistration,
+                RealtySaleValue = realtyEntity.RealtySaleValue,
+                Address = ConvertersManager.AddressConverter.Convert(realtyEntity.Address)
+            };
+
+            return realtyDomain;
+        }
+
+        public RealtyEntity Convert(RealtyDomain realtyDomain)
+        {
+            if (realtyDomain == null)
+                return null;
+
+            var realtyEntity = new RealtyEntity()
+            {
+                BeneficiaryId = realtyDomain.BeneficiaryId,
+                IsDeleted = realtyDomain.IsDeleted,
+                RealtyConstructionDate = realtyDomain.RealtyConstructionDate,
+                RealtyMarketValue = realtyDomain.RealtyMarketValue,
+                RealtyMunicipalRegistration = realtyDomain.RealtyMunicipalRegistration,
+                RealtySaleValue = realtyDomain.RealtySaleValue,
+                Address = ConvertersManager.AddressConverter.Convert(realtyDomain.Address)
+            };
+
+            return realtyEntity;
+        }
+    }
+}
