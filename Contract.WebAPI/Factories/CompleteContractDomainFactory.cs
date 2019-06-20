7eb1964e2ab9058fa07704373bf1ae3862d38be0
Ariel Molina <ariel.molina@gft.com>
2019-06-20 11:19:24 -0300

    Factories creation.

---
+++ Contract.WebAPI/Factories/CompleteContractDomainFactory.cs
@@ -0,0 +1,49 @@
+﻿using Backend.Core.Domains;
+using Contract.WebAPI.Factories.Interfaces;
+using Contract.WebAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contract.WebAPI.Factories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CompleteContractDomainFactory : IFactory<CompleteContractDomain, ContractViewModel>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public CompleteContractDomain Create(ContractViewModel contractViewModel)
+        {
+            var contract = new ContractDomain()
+            {
+                ContractCategory = contractViewModel.Category,
+                ContractDeleted = false,
+                ContractExpiryDate = contractViewModel.ExpiryDate,
+                ContractType = contractViewModel.Type,
+                ContractId = Guid.Empty
+            };
+            return new CompleteContractDomain()
+            {
+                Contract = contract,
+                SignedContract = new SignedContractDomain()
+                {
+                    ContractId = Guid.Empty,
+                    ContractIndividualIsActive = contractViewModel.IsActive,
+                    IndividualId = contractViewModel.ContractHolderId,
+                    SignedContractContract = contract,
+                    SignedContractId = contractViewModel.SignedContractId
+                },
+                Individuals = contractViewModel.Individuals,
+                MobileDevices = contractViewModel.MobileDevices,
+                Pets = contractViewModel.Pets,
+                Realties = contractViewModel.Realties,
+                Vehicles = contractViewModel.Vehicles,
+            };
+        }
+    }
+}
---
+++ Contract.WebAPI/Factories/ContractViewModelFactory.cs
@@ -0,0 +1,28 @@
+﻿using Backend.Core.Domains;
+using Contract.WebAPI.Factories.Interfaces;
+using Contract.WebAPI.ViewModels;
+
+namespace Contract.WebAPI.Factories
+{
+    public class ContractViewModelFactory : IFactory<ContractViewModel, CompleteContractDomain>
+    {
+        public ContractViewModel Create(CompleteContractDomain completeContractDomain)
+        {
+            return new ContractViewModel()
+            {
+                Category = completeContractDomain.Contract.ContractCategory,
+                ContractHolder = completeContractDomain.SignedContract.SignedContractIndividual,
+                ContractHolderId = completeContractDomain.SignedContract.IndividualId,
+                ExpiryDate = completeContractDomain.Contract.ContractExpiryDate,
+                Individuals = completeContractDomain.Individuals,
+                Pets = completeContractDomain.Pets,
+                MobileDevices = completeContractDomain.MobileDevices,
+                Realties = completeContractDomain.Realties,
+                Vehicles = completeContractDomain.Vehicles,
+                IsActive = completeContractDomain.SignedContract.ContractIndividualIsActive,
+                SignedContractId = completeContractDomain.SignedContract.SignedContractId,
+                Type = completeContractDomain.Contract.ContractType
+            };
+        }
+    }
+}
